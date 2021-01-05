daec193b9b0dfb4a2064f16dac626dce3b971a33
zenginberkecan <60400979+zenginberkecan@users.noreply.github.com>
2021-01-05 12:43:53 +0300

    Update sexs.py

--- sexs.py
+++ sexs.py
@@ -9,16 +9,16 @@ async def my_background_task():
 
     while not client.is_closed():
       
-      if open("text.txt", "r").read() == "1":
-            await channel.send("Fuck, get back to lesson.")
-            await channel.send(file=discord.File("ss.png"))
-            open("text.txt", "w").write("0")
+      if open("text.txt", "r").read() == "1":                   ## If the bot reads "1" in the text file,
+            await channel.send("Fuck, get back to lesson.")     ## it sends the message
+            await channel.send(file=discord.File("ss.png"))     ## and the screenshot.
+            open("text.txt", "w").write("0")                    ## After that, it changes the "1" into a "0".
             await asyncio.sleep(1)
             
         else:
             await asyncio.sleep(1)
 
-        if open("text.txt", "r").read() == "2":
+        if open("text.txt", "r").read() == "2":                 ## You can write "2" in the text file and save it to stop the bot. Not necessary, you can stop it in any way.
             await channel.send("Bip bop. Stopping the bot.")
             open("text.txt", "w").write("0")
             await client.close() ## This gives an error message. But eventually it works ^^
